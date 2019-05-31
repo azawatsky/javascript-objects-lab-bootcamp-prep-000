d4c2d0eb8fb1552cca04f6e7529a65da55f6c865
Alex Zawatsky <alexzawatsky@yahoo.com>
2019-05-31 15:49:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,4 +11,5 @@ function destructivelyUpdateObjectWithKeyAndValue(object,key,value) {
 function deleteFromObjectByKey(object,key) {
   var recipesClone = Object.assign({},object)
   delete recipesClone[key]
+  return recipesClone
 }
